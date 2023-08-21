c8847e69bcc8dab2dabf690b2fb4b980c149fb84
radhakrishan <radhakrishan.r@ril.com>
2023-08-21 22:15:54 +0530

    Initial Commit

---
+++ Worklog/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Worklog
+//
+//  Created by Mobile Programming  on 21/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Worklog/WorklogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorklogApp.swift
+//  Worklog
+//
+//  Created by Mobile Programming  on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WorklogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
